0aeefaa16dab8bf2e93a72e4be6ae58a2e951a21
BonthuLavanya <bonthulavanya04@gmail.com>
2020-03-06 16:30:03 +0530

    My Commit

---
+++ Collections/src/main/java/com/epam2/Collections/App.java
@@ -0,0 +1,35 @@
+package com.epam2.Collections;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{   
+  public static void main(String[] args)
+  {
+    Customarraylist l = new Customarraylist();
+    l.add(1.99);
+    l.add(2.99);
+    l.add(3.99);
+    l.add(4.99);
+    l.add(5.99);
+    for(int i=0;i<l.size();i++)
+    {
+      System.out.print(l.get(i)+" ");
+    }
+    l.add(6.99);
+    l.add(7.99);
+    l.add(8.99);
+    l.add(10.99);
+    l.add(11.99);
+    l.add(12.99);
+    System.out.println("Element at Index 5:"+l.get(5));
+    System.out.println("List size: "+l.size());
+    l.remove(2);
+    for(int i=0;i<l.size();i++)
+    {
+      System.out.print(l.get(i)+" ");
+    }
+  }   
+} 
